c22b27c94c38073e496977738edf8bde740f0aec
Justin Pena <arcboundm9@yahoo.com>
2019-07-19 14:04:26 +0800

    Added basic CameraX image preview

--- app/src/main/java/cipher/com/cameraxqrscanner/MainActivity.kt
+++ app/src/main/java/cipher/com/cameraxqrscanner/MainActivity.kt
@@ -1,12 +1,75 @@
 package cipher.com.cameraxqrscanner
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Rational
+import android.widget.Toast
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import cipher.com.utils.AutoFitPreviewBuilder
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+private const val REQUEST_CODE_PERMISSIONS = 10
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+class MainActivity : AppCompatActivity(), LifecycleOwner {
+
+    private var preview: Preview? = null
+    private var lensFacing = CameraX.LensFacing.BACK
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            camera_viewfinder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+    }
+
+    private fun startCamera(){
+        // Get screen metrics used to setup camera for full screen resolution
+        val metrics = DisplayMetrics().also { camera_viewfinder.display.getRealMetrics(it) }
+        val screenAspectRatio = Rational(metrics.widthPixels, metrics.heightPixels)
+
+        // Create configuration object for the preview
+        val previewConfig = PreviewConfig.Builder().apply {
+            setLensFacing(lensFacing)
+            setTargetAspectRatio(screenAspectRatio)
+            setTargetRotation(camera_viewfinder.display.rotation)
+        }.build()
+
+        // Build the viewfinder use case
+        preview = AutoFitPreviewBuilder.build(previewConfig, camera_viewfinder)
+
+        CameraX.bindToLifecycle(this, preview)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                camera_viewfinder.post { startCamera() }
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 }
