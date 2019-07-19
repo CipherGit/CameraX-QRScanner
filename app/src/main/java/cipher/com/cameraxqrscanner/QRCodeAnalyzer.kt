347eaa721da483d33d64eb8fa5da616752aa1f0b
Justin Pena <arcboundm9@yahoo.com>
2019-07-19 17:39:16 +0800

    Added Orientations Array

--- app/src/main/java/cipher/com/cameraxqrscanner/QRCodeAnalyzer.kt
+++ app/src/main/java/cipher/com/cameraxqrscanner/QRCodeAnalyzer.kt
@@ -1,5 +1,7 @@
 package cipher.com.cameraxqrscanner
 
+import android.util.SparseIntArray
+import android.view.Surface
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.google.firebase.ml.vision.FirebaseVision
@@ -10,6 +12,7 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage
 
 class QRCodeAnalyzer(private val qrListener: QRCodeListener) : ImageAnalysis.Analyzer {
 
+    private val ORIENTATIONS = SparseIntArray()
     private val firebaseQRDetector : FirebaseVisionBarcodeDetector
 
     init {
@@ -17,12 +20,17 @@ class QRCodeAnalyzer(private val qrListener: QRCodeListener) : ImageAnalysis.Ana
             .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE)
             .build()
         firebaseQRDetector = FirebaseVision.getInstance().getVisionBarcodeDetector(options)
+
+        ORIENTATIONS.append(90, Surface.ROTATION_0)
+        ORIENTATIONS.append(0, Surface.ROTATION_90)
+        ORIENTATIONS.append(270, Surface.ROTATION_180)
+        ORIENTATIONS.append(180, Surface.ROTATION_270)
     }
 
     override fun analyze(image: ImageProxy?, rotationDegrees: Int) {
         if(image != null){
             image.image?.let {
-                val fireBaseImage = FirebaseVisionImage.fromMediaImage(it, rotationDegrees)
+                val fireBaseImage = FirebaseVisionImage.fromMediaImage(it, ORIENTATIONS[rotationDegrees])
                 firebaseQRDetector.detectInImage(fireBaseImage)
                     .addOnSuccessListener { results ->
                         qrListener.onQRReadSuccess(results)
